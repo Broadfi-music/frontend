240cfb852b87dbeef9d3fd53b0b4daccca085dd1
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2025-03-05 05:32:06 +0100

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import { TonConnectButton, useTonWallet } from "@tonconnect/ui-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./styles/nav.css";
-import NavLinks from "./NavLinks";
 
 function Navbar() {
 	const [active, setActive] = useState(false);
+    const wallet = useTonWallet();
+	const optimizedAddress = wallet
+		? wallet.account.address.slice(0, 15) + "..."
+		: "";
+
 	return (
 		<>
 			<img className="blur" src="/Effects/blur.png" alt="" />
@@ -21,7 +25,32 @@ function Navbar() {
 						</Link>
 					</div>
 					<div className="navbar-nav">
-						<NavLinks setActive={setActive} />
+						<Link
+							to={"/contest"}
+							className="nav-link"
+							onClick={() => setActive(false)}
+						>
+							Contest
+						</Link>
+						<Link
+							to={"/tokenmics"}
+							className="nav-link"
+							onClick={() => setActive(false)}
+						>
+							Tokenmics
+						</Link>
+						<Link
+							to={"/discover"}
+							className="nav-link"
+							onClick={() => setActive(false)}
+						>
+							Discover
+						</Link>
+						{wallet ? (
+							<p>Connected: {optimizedAddress}</p>
+						) : (
+							<TonConnectButton className="btn btn-danger tonbtn" />
+						)}
 					</div>
 					<div
 						className={active ? "menu-icon active" : "menu-icon"}
