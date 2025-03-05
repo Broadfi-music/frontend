5250682c43890706c0c06f2b932739a2fdb861c8
Judah Oyedele <oyedeleinioluwajudah@gmail.com>
2025-03-05 04:48:04 +0100

    made links better

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./styles/nav.css";
-import { TonConnectButton, useTonWallet } from "@tonconnect/ui-react";
+import NavLinks from "./NavLinks";
 
 function Navbar() {
 	const [active, setActive] = useState(false);
-	const wallet = useTonWallet();
-
-	const optimizedAddress = wallet
-		? wallet.account.address.slice(0, 15) + "..."
-		: "";
-
 	return (
 		<>
 			<img className="blur" src="/Effects/blur.png" alt="" />
@@ -27,20 +21,7 @@ function Navbar() {
 						</Link>
 					</div>
 					<div className="navbar-nav">
-						<Link to={"/contest"} className="nav-link">
-							Contest
-						</Link>
-						<Link to={"/tokenmics"} className="nav-link">
-							Tokenmics
-						</Link>
-						<Link to={"/discover"} className="nav-link">
-							Discover
-						</Link>
-						{wallet ? (
-							<p>Connected: {optimizedAddress}</p>
-						) : (
-							<TonConnectButton className="btn btn-danger tonbtn" />
-						)}
+						<NavLinks setActive={setActive} />
 					</div>
 					<div
 						className={active ? "menu-icon active" : "menu-icon"}
@@ -52,32 +33,7 @@ function Navbar() {
 				</div>
 			</div>
 			<div className={active ? "nav-menu active" : "nav-menu"}>
-				<Link
-					to={"/contest"}
-					className="nav-link"
-					onClick={() => setActive(false)}
-				>
-					Contest
-				</Link>
-				<Link
-					to={"/tokenmics"}
-					className="nav-link"
-					onClick={() => setActive(false)}
-				>
-					Tokenmics
-				</Link>
-				<Link
-					to={"/discover"}
-					className="nav-link"
-					onClick={() => setActive(false)}
-				>
-					Discover
-				</Link>
-				{wallet ? (
-					<p>Connected: {optimizedAddress}</p>
-				) : (
-					<TonConnectButton className="btn btn-danger tonbtn" />
-				)}
+				<NavLinks setActive={setActive} />
 			</div>
 		</>
 	);
